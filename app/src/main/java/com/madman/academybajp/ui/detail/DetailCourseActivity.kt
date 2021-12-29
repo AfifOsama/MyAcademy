e7ddac808abab75bfe6b70ce4b25c0b88fc19b9e
AfifOsama <afifosama66@gmail.com>
2021-12-29 23:10:09 +0700

    layouting the project

--- app/src/main/java/com/madman/academybajp/ui/detail/DetailCourseActivity.kt
+++ app/src/main/java/com/madman/academybajp/ui/detail/DetailCourseActivity.kt
@@ -2,27 +2,24 @@ package com.madman.academybajp.ui.detail
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.snackbar.Snackbar
 import com.madman.academybajp.databinding.ActivityDetailCourseBinding
+import com.madman.academybajp.databinding.ContentDetailCourseBinding
 
 class DetailCourseActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityDetailCourseBinding
+    private lateinit var contentDetailBinding: ContentDetailCourseBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityDetailCourseBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        val detailCourseBinding = ActivityDetailCourseBinding.inflate(layoutInflater)
+        contentDetailBinding = detailCourseBinding.detailContent
+        setContentView(detailCourseBinding.root)
 
-        setSupportActionBar(binding.toolbar)
+        setSupportActionBar(detailCourseBinding.toolbar)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
 
 
